6ac724fcb2c853261dd97c3b9f38d3b22e3b3f84
Girithar R <girithargirithar089@gmail.com>
2026-02-24 04:05:17 +0000

    signup page created

--- _template/_signup.php
+++ _template/_signup.php
@@ -1,26 +1,6 @@
-<?php
 
-    $username = $_POST['email_address'];
-    $password = $_POST['password'];
-
-    $result =validate_user($username,$password);
-    if($result){
-       ?>
-<main class="container">
-    <div class="bg-light p-5 rounded mt-3">
-        <h1>Login Success</h1>
-        <p class="lead">This example is a quick exercise to do basic login with html forms.</p>
-    </div>
-</main>
-<?php
-} 
-else {
- 
-        ?>
-
-
-<main class="form-signin">
-      <form method="post" action="/app/login.php">
+<main class="form-signup">
+      <form method="post" action="test.php">
         <img
           class="mb-4"
           src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg"
@@ -28,7 +8,7 @@ else {
           width="72"
           height="70"
         />
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <h1 class="h3 mb-3 fw-normal">Please sign up </h1>
         <div class="form-floating">
           <input
             type="email"
@@ -37,18 +17,41 @@ else {
             name="email_address"
             placeholder="name@example.com"
           />
-          <label for="floatingInput">Email address</label>
+
+          <label for="floatingInput">email adddress</label>
         </div>
         <div class="form-floating">
           <input
             type="password"
             class="form-control"
-            id="floatingPassword"
+            id="floatingpassword"
             name="password"
-            placeholder="Password"
+            placeholder="password"
           />
-          <label for="floatingPassword">Password</label>
+          <label for="floatingpassword">Password</label>
         </div>
+        
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingusername"
+            name="user_name"
+            placeholder="User Name"
+          />
+          <label for="floatingusername">User Name</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingphone"
+            name="phone"
+            placeholder="phone"
+          />
+          <label for="floatingphone">Phone</label>
+        </div>
+        
         <div class="form-check text-start my-3">
           <input class="form-check-input"
             type="checkbox"
@@ -63,7 +66,4 @@ else {
           Sign in
         </button>
       </form>
-</main>
-
-<?php
-}
+</main> 
--- _template/head.php
+++ _template/head.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
   <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta
+      name="author"
+      content="Mark Otto, Jacob Thornton, and Bootstrap contributors"
+    />
+    <meta name="generator" content="Astro v5.13.2" />
+    <meta name="theme-color" content="#712cf9" />
     <title>webgram 🩵</title>
     <link
       rel="canonical"
@@ -8,19 +17,12 @@
     />
     <script src="/app/assets/js/color-modes.js"></script>
     <link href="/app/assets/dist/css/bootstrap.min.css" rel="stylesheet" />
-   
     
     
-
-<html lang="en" data-bs-theme="auto">
-  <head>
+  
+    <title>sign-up page</title>
+   <link rel="stylesheet" href="/app/css/signup.css">
+   <link rel="stylesheet" href="/app/css/login.css">
     
-    <title>sign-in page</title>
-    <link
-      rel="canonical"
-      href="https://getbootstrap.com/docs/5.3/examples/sign-in/"
-    />
-    <script src="/app/assets/js/color-modes.js"></script>
-    <link href="/app/assets/dist/css/bootstrap.min.css" rel="stylesheet" />
- 
+</head>
 
