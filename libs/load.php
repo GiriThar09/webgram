16c42ff212ac6fcba19bc0bbd01019511dedb11c
Girithar R <girithargirithar089@gmail.com>
2026-03-08 11:24:17 +0000

    php session testing

--- libs/load.php
+++ libs/load.php
@@ -1,10 +1,11 @@
 <?php
 
-
+include_once 'includes/session.class.php';
 include_once 'includes/Database.class.php';
 include_once 'includes/User.class.php';
 include_once 'includes/Mic.class.php';
 
+Session::start();
 function load_template($name)
 {
     include $_SERVER['DOCUMENT_ROOT']."/app/_template/$name";
